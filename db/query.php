1150544a65c27eb0149dcb6841e7ebc64bbdb034
Richard V. 121 <ricardo@121ecommerce.com>
2017-09-29 00:03:27 -0300

    Examples DB

--- db/query.php
+++ db/query.php
@@ -4,14 +4,20 @@
     try {
         $db     = new PDO("mysql:host=127.0.0.1;dbname=employees", "root", "ksk()sk2312");
         $empNro = 10002;
+        /** @var PDOStatement $query */
         $query  = $db->query("SELECT * FROM employees WHERE emp_no=$empNro");
-    
-        var_dump($query);
+//        var_dump($query);
+
+        /** @var PDOStatement $result */
         $result = $query->fetchAll(PDO::FETCH_ASSOC);
+        
+//        var_dump($result->rowCount());
+        
         var_dump($result);
     } catch (PDOException  $e) {
-        echo "Error: " . $e;
+        var_dump($e);
+        echo "Error: " . $e->getMessage();
     }
-
+echo 'hola';
     ?>
 </pre>
--- db/statement.php
+++ db/statement.php
@@ -2,9 +2,14 @@
     <?php
 
     try {
+        $sql
+                = 'SELECT first_name, last_name
+                   FROM fruit
+                   WHERE emp_no > :minor AND emp_no <:mayor';
         $db     = new PDO("mysql:host=127.0.0.1;dbname=employees", "root", "ksk()sk2312");
-        $empNro = 10002;
-        $query  = $db->query("SELECT * FROM employees WHERE emp_no=$empNro");
+        $minor = 10002;
+        $mayor = 10010;
+        $query  = $db->prepare($sql, array(':minor' => $minor, ':mayor' => $mayor));
     
         var_dump($query);
         $result = $query->fetchAll(PDO::FETCH_ASSOC);
@@ -15,3 +20,16 @@
 
     ?>
 </pre>
+
+<?php
+/* Ejecuta una sentencia preparada pasando un array de valores */
+$sql
+     = 'SELECT name, colour, calories
+    FROM fruit
+    WHERE calories < :calories AND colour = :colour';
+$sth = $dbh->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+$sth->execute(array(':calories' => 150, ':colour' => 'red'));
+$red = $sth->fetchAll();
+$sth->execute(array(':calories' => 175, ':colour' => 'yellow'));
+$yellow = $sth->fetchAll();
+?>
